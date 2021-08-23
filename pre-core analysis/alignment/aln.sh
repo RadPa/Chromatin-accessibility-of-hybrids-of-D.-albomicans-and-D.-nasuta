3773f279b5f978cf29f7ad414f9fb0180cec8a36
RadPa <49912152+RadPa@users.noreply.github.com>
2021-08-23 16:49:24 +0530

    Update aln.sh

--- pre-core analysis/alignment/aln.sh
+++ pre-core analysis/alignment/aln.sh
@@ -13,10 +13,12 @@ j=`echo "$i" | sed 's/_1/_2/g'`
 
 k=`echo "$i" | sed 's/_1.fastq.gz//g'` 
 
+FA=/home/radhika/atac/info-g/GCF_009650485.1_drosAlbom15112-1751.03v1_genomic.fna
+
 echo mapping "$k"
 ##Mapping and format conversions##
 
-/home/radhika/bowtie2-2.4.2-sra-linux-x86_64/bowtie2 -p 32 --very-sensitive -X 1000 --seed 21 -x GCF_009650485.1_drosAlbom15112-1751.03v1_genomic.fna -1 "$i" -2 "$j" | /home/radhika/samtools-1.11/samtools view -b -o "$k".bam
+/home/radhika/bowtie2-2.4.2-sra-linux-x86_64/bowtie2 -p 32 --very-sensitive -X 1000 --seed 21 -x $FA -1 "$i" -2 "$j" | /home/radhika/samtools-1.11/samtools view -b -o "$k".bam
 /home/radhika/samtools-1.11/samtools sort "$k".bam -o "$k".sbam 
 
 echo samtools "$k"
