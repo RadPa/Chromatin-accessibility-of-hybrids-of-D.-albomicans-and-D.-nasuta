8d0511462dac7183f3c01a1a1802cbecff88565a
RadPa <49912152+RadPa@users.noreply.github.com>
2021-10-21 14:37:17 +0530

    Update aln.sh

--- pre-core analysis/alignment/aln.sh
+++ pre-core analysis/alignment/aln.sh
@@ -2,39 +2,41 @@
 #Z840 workstation
 cd /home/radhika/atac/aln/testes/
 
-#### Bowtie version 2.4.2 ####
+#### Bowtie version 2.4.4 ####
+
 ##  Genome indexing ##
- 
-#30.64hrs on 10.01.21
+# 1min 32s in Z840 #
+/home/radhika/bowtie2-2.4.4-linux-x86_64/bowtie2-build --threads 32 -f GCF_009650485.1_drosAlbom15112-1751.03v1_genomic.fna /home/radhika/atac/aln/testes/GCF_009650485.1_drosAlbom15112-1751.03v1_genomic.fna
+
 for i in `ls *_1.fastq.gz`
 do 
 
 j=`echo "$i" | sed 's/_1/_2/g'` 
+k=`echo "$i" | sed 's/_ct_1.fastq.gz//g'` 
 
-k=`echo "$i" | sed 's/_1.fastq.gz//g'` 
-
-FA=/home/radhika/atac/info-g/GCF_009650485.1_drosAlbom15112-1751.03v1_genomic.fna
-
+## Mapping, format conversions and Stats ##
+# Mapping #
 echo mapping "$k"
-##Mapping and format conversions##
-#Indexing#
-/home/radhika/bowtie2-2.4.2-sra-linux-x86_64/bowtie2-build --threads 16 -f $FA
-#Mapping#
-/home/radhika/bowtie2-2.4.2-sra-linux-x86_64/bowtie2 -p 32 --very-sensitive -X 1000 --seed 21 -x $FA -1 "$i" -2 "$j" | /home/radhika/samtools-1.11/samtools view -b -o "$k".bam
-/home/radhika/samtools-1.11/samtools sort "$k".bam -o "$k".sbam 
+/home/radhika/bowtie2-2.4.4-linux-x86_64/bowtie2 --very-sensitive -X 700 -p 16 --seed 21 -x GCF_009650485.1_drosAlbom15112-1751.03v1_genomic.fna -1 "$i" -2 "$j"| /home/radhika/samtools-1.13/samtools view -@ 16 -bo "$k".bam
 
-echo samtools "$k"
-##Statistics## 
-/home/radhika/samtools-1.11/samtools index "$k".sbam
-/home/radhika/samtools-1.11/samtools flagstat "$k".sbam > "$k".sbam.stat 
+# Tidying the data #
+echo tidying "$k"
+/home/radhika/samtools-1.13/samtools sort -@ 16 "$k".bam -o "$k".sbam
+/home/radhika/samtools-1.13/samtools index -@ 16 "$k".sbam
+/home/radhika/samtools-1.13/samtools flagstat "$k".sbam
 
-##Filtering .bam##
-echo marking duplicates
-##Duplicates marked by picardtools## 
-/home/radhika/gatk-4.1.9.0/./gatk MarkDuplicates -I "$k".sbam -O "$k".dup.bam -M "$k".dupmatr.txt -REMOVE_DUPLICATES false 
-/home/radhika/samtools-1.11/samtools sort "$k".dup.bam -o "$k".sdup.bam
-/home/radhika/samtools-1.11/samtools index "$k".sdup.bam
-/home/radhika/samtools-1.11/samtools flagstat "$k".sdup.bam > "$k".sdup.stat
+## Filtering data ##
+# Removing Mitochondrial reads #
+echo removing mito reads of "$k"
+/home/radhika/samtools-1.13/samtools idxstats "$k".sbam| cut -f 1 | grep -v NC_027937.1 | xargs /home/radhika/samtools-1.13/samtools view -@ 16 -b "$k".sbam|/home/radhika/samtools-1.13/samtools sort -@ 16 -o "$k".mtr.bam
+/home/radhika/samtools-1.13/samtools index -@ 16 "$k".mtr.bam
+/home/radhika/samtools-1.13/samtools flagstat "$k".mtr.bam
 
+# Marking Duplicates #
+echo marking duplicates of "$k"
+/home/radhika/gatk-4.2.2.0/gatk MarkDuplicates -I "$k".mtr.bam -O "$k".mtrdm.bam -M "$k".dupmatr.txt -REMOVE_DUPLICATES false
+/home/radhika/samtools-1.13/samtools index -@ 16 "$k".mtrdm.bam
+/home/radhika/samtools-1.13/samtools flagstat "$k".mtrdm.bam
+/home/radhika/gatk-4.2.2.0/gatk CollectInsertSizeMetrics -I "$k".mtrdm.bam -O "$k".mtrdm.is_metrics.txt -H "$k".mtrdm.is_histogram.pdf -M 0.5
 echo "$k" done
 done
