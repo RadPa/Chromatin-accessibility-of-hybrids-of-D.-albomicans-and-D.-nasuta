7d500260e28221bc49460aa2bcb4f6f28b04cec5
RadPa <49912152+RadPa@users.noreply.github.com>
2021-10-26 14:52:57 +0530

    Update aln.sh

--- pre-core analysis/alignment/aln.sh
+++ pre-core analysis/alignment/aln.sh
@@ -26,17 +26,18 @@ echo tidying "$k"
 /home/radhika/samtools-1.13/samtools flagstat "$k".sbam
 
 ## Filtering data ##
-# Removing Mitochondrial reads #
-echo removing mito reads of "$k"
-/home/radhika/samtools-1.13/samtools idxstats "$k".sbam| cut -f 1 | grep -v NC_027937.1 | xargs /home/radhika/samtools-1.13/samtools view -@ 16 -b "$k".sbam|/home/radhika/samtools-1.13/samtools sort -@ 16 -o "$k".mtr.bam
-/home/radhika/samtools-1.13/samtools index -@ 16 "$k".mtr.bam
-/home/radhika/samtools-1.13/samtools flagstat "$k".mtr.bam
-
 # Marking Duplicates #
 echo marking duplicates of "$k"
-/home/radhika/gatk-4.2.2.0/gatk MarkDuplicates -I "$k".mtr.bam -O "$k".mtrdm.bam -M "$k".dupmatr.txt -REMOVE_DUPLICATES false
-/home/radhika/samtools-1.13/samtools index -@ 16 "$k".mtrdm.bam
-/home/radhika/samtools-1.13/samtools flagstat "$k".mtrdm.bam
-/home/radhika/gatk-4.2.2.0/gatk CollectInsertSizeMetrics -I "$k".mtrdm.bam -O "$k".mtrdm.is_metrics.txt -H "$k".mtrdm.is_histogram.pdf -M 0.5
+/home/radhika/gatk-4.2.2.0/gatk MarkDuplicates -I "$k".sbam -O "$k".sdm.bam -M "$k".sdmatr.txt -REMOVE_DUPLICATES false
+/home/radhika/samtools-1.13/samtools index -@ 16 "$k".sdm.bam
+/home/radhika/samtools-1.13/samtools flagstat "$k".sdm.bam
+/home/radhika/gatk-4.2.2.0/gatk CollectInsertSizeMetrics -I "$k".sdm.bam -O "$k".sdm.is_metrics.txt -H "$k".sdm.is_histogram.pdf -M 0.5
+
+# Removing Mitochondrial reads #
+echo removing mito reads of "$k"
+/home/radhika/samtools-1.13/samtools idxstats "$k".sdm.bam| cut -f 1 | grep -v NC_027937.1 | xargs /home/radhika/samtools-1.13/samtools view -@ 16 -b "$k".sdm.bam|/home/radhika/samtools-1.13/samtools sort -@ 16 -o "$k".sdm-mr.bam
+/home/radhika/samtools-1.13/samtools index -@ 16 "$k".sdm-mr.bam
+/home/radhika/samtools-1.13/samtools flagstat "$k".sdm-mr.bam
+
 echo "$k" done
 done
