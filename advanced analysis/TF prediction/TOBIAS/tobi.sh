9d7f85efd3ff5cd51c1714d016c275fe1fef0c09
RadPa <49912152+RadPa@users.noreply.github.com>
2021-10-27 19:29:58 +0530

    Update tobi.sh

--- advanced analysis/TF prediction/TOBIAS/tobi.sh
+++ advanced analysis/TF prediction/TOBIAS/tobi.sh
@@ -4,18 +4,21 @@
 
 source /home/radhika/py3-env/bin/activate
 FA=/home/radhika/atac/tff/GCF_009650485.1_drosAlbom15112-1751.03v1_genomic.fna
-BED=/home/radhika/atac/tff/tobi/peaks.nfr.shm1.bed
-BLA=/home/radhika/atac/info-g/Dalb-at.v1.blacklist.bed
-BLN=/home/radhika/atac/info-g/Dnas-at-dalb.v1.blacklist.bed
-JAS=/home/radhika/atac/tff/20210819_JASPAR2018_combined_matrices_13546_jaspar.txt
+BED=/home/radhika/atac/tff/tobi/dac.tes.x7.peak.bed
+BLA=/home/radhika/atac/info-g/Dalb-pub.sm.v1.blacklist.bed
+JAS=/home/radhika/atac/tff/JASPAR2020_CORE_insects_non-redundant_pfms_jaspar.txt
+JASCM=/home/radhika/atac/tff/20211027072622_JASPAR2022_combined_matrices_25153_jaspar.txt
 
 echo ATACorrect 
-TOBIAS ATACorrect --bam AAT.dupr.bam --genome "$FA" --peaks "$BED" --blacklist "$BLA" --cores 16 --prefix AATd
-TOBIAS ATACorrect --bam ANT.dupr.bam --genome "$FA" --peaks "$BED" --blacklist "$BLN" --cores 16 --prefix ANTd
+TOBIAS ATACorrect --bam AAT.x7.mer.bam --genome "$FA" --peaks "$BED" --blacklist "$BLA" --cores 16 --prefix AATx7
+TOBIAS ATACorrect --bam ANT.x7.mer.bam --genome "$FA" --peaks "$BED" --blacklist "$BLA" --cores 16 --prefix ANTx7
 
 echo footprint-scores
-TOBIAS ScoreBigwig --signal AATd_corrected.bw --regions "$BED" --output AATd_footprints.bw --cores 16
-TOBIAS ScoreBigwig --signal ANTd_corrected.bw --regions "$BED" --output ANTd_footprints.bw --cores 16
+TOBIAS ScoreBigwig --signal AATx7_corrected.bw --regions "$BED" --output AATx7_footprints.bw --cores 16
+TOBIAS ScoreBigwig --signal ANTx7_corrected.bw --regions "$BED" --output ANTx7_footprints.bw --cores 16
 
 echo BINDetect
-TOBIAS BINDetect --signals AATd_footprints.bw ANTd_footprints.bw --peaks "$BED" --motifs "$JAS" --genome "$FA" --cond-names alb nas --prefix diff --outdir dudf --cores 16 
+TOBIAS BINDetect --signals AATx7_footprints.bw ANTx7_footprints.bw --peaks "$BED" --motifs "$JAS" --genome "$FA" --cond-names alb nas --prefix dac --outdir dac-nr --cores 16
+TOBIAS BINDetect --signals AATx7_footprints.bw ANTx7_footprints.bw --peaks "$BED" --motifs "$JASCM" --genome "$FA" --cond-names alb nas --prefix dac --outdir dac-cm --cores 16
+
+echo TOBIAS done
