e5f70c15566421493554afa50d41070a075c3b68
RadPa <49912152+RadPa@users.noreply.github.com>
2022-01-01 12:51:26 +0530

    Update prelude.sh

--- advanced analysis/GO enrichment/topGO/prelude.sh
+++ advanced analysis/GO enrichment/topGO/prelude.sh
@@ -1,19 +1,11 @@
-#From the res of DESeq2 to bedtools intersect
-/home/radhika/bedtools2/bin/bedtools intersect -a /home/radhika/atac/tff/GCF_009650485.1_drosAlbom15112-1751.03v1_genomic.agat.sort.gff -b nfr.tes.des.bed > nfr.tes.des.int.bed
+#Intersecting DACs of DESeq2 with genes only with formating
+/home/hp1/radhika/bedtools2/bin/bedtools intersect -wb -a <(awk '$3=="gene"' ../data/GCF_009650485.1_drosAlbom15112-1751.03v1_genomic.agat.sort.gff) -b <(cut -f 1-3 nfr.tes.des.q.bed) |awk -v OFS="\t" -F '["\t"";"]' ' {gsub(/ID=gene-/,"")} {print $15,$16,$17,$9}' > nfr.tes.des.int.bed 
 
-#For our need
-awk '$3=="gene" {print $1,$4,$5,$9}' nfr.tes.des.int.bed|awk -F ';' '{print $1}'|sed 's/ID=gene-//'|awk -v OFS="\t" '{print $4,$1,$2,$3}'>nfr.tes.des.g.bed 
+#Intersect int bed with pvalues from DESeq2
+/home/hp1/radhika/bedtools2/bin/bedtools intersect -wb -a nfr.tes.des.q.bed -b nfr.tes.des.int.bed|awk -v OFS="\t" '{print $1,$2,$3,$10,$4,$5,$6}' > nfr.tes.des.go.bed
 
-awk 'NR==FNR {end[$4]; next} ($3 in end)' nfr.tes.des.g.bed nfr.tes.des.txt > nfr.tes.des.go.txt 
-
-cut -f 1 nfr.tes.des.g.bed > genes 
-
-paste genes.txt nfr.tes.des.go.txt > nfr.tes.des.go 
-
-cut -f 1,5-10 nfr.tes.des.go > nfr.tes.des.go.r.txt 
+#Manually removing the duplicates and kept only with higher degree of log2foldchange 
+nfr.tes.des.gom.bed
 
 #GO annotation from uniprot 
-
 sed -ie '1d'  -e  's/\;/,/g' Dalb-GO.txt  
-
-mv nfr.tes.des.go.r.txt nfr.tes.des.genelist 
